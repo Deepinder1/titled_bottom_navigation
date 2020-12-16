93e76a7a57f0f7a49d988524f4373a9a9c98cb07
Deepinder Singh <deep6421@gmail.com>
2020-12-16 17:12:49 +0530

    initialized

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:titled_navigation_bar/titled_navigation_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Titled Bar',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<TitledNavigationBarItem> items = [
+    TitledNavigationBarItem(title: Text('Home'), icon: Icons.home),
+    TitledNavigationBarItem(title: Text('Search'), icon: Icons.search),
+    TitledNavigationBarItem(title: Text('Bag'), icon: Icons.card_travel),
+    TitledNavigationBarItem(title: Text('Orders'), icon: Icons.shopping_cart),
+    TitledNavigationBarItem(title: Text('Profile'), icon: Icons.person_outline),
+  ];
+
+  bool navBarMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titled Bottom Bar"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Reversed mode:"),
+            Switch(
+              value: navBarMode,
+              onChanged: (v) {
+                setState(() => navBarMode = v);
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: TitledBottomNavigationBar(
+        onTap: (index) => print("Selected Index: $index"),
+        reverse: navBarMode,
+        curve: Curves.easeInBack,
+        items: items,
+        activeColor: Colors.red,
+        inactiveColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
